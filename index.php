ca2055b3a368bcf7884df47c8b1e172e1e5d2fdc
indykoning <indykoning@gmail.com>
2017-03-06 14:43:46 +0100

    basis

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+require_once 'classes/Login.php';
+$action = isset($_GET['page']) ? $_GET['page'] : '';
+$login = new Login('users', 'id', 'email', 'wachtwoord', 'key');
+if(!empty($_POST['registreren'])){
+    $login->register($_POST['voornaam'], $_POST['achternaam'], $_POST['wachtwoord'],$_POST['telefoon'], $_POST['e-mail'], $_POST['naam-organisatie'], $_POST['website'], $_POST['type'], $_POST['plaats']);
+}
+if (isset($_GET['logout'])){
+    $login->logout();
+}
+define('LOGGED_IN', $login->loggedin());
+if($action != ''){
+if(file_exists('views/'. $action . '.php')){
+include_once 'views/'. $action . '.php';
+}else{
+include_once 'views/404.php';
+}
+}else{
+    include_once 'views/home.php';
+}

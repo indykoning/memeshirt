17b3884f1f896cd188a0ae13052a136f94d33031
indykoning <indykoning@gmail.com>
2017-03-13 14:21:20 +0100

    index code verkort

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
+require_once "includes/config.php";
+require_once  "includes/db.php";
+
 define('TITLE', 'MemeShirt');
 //require_once 'classes/Login.php';
-$action = isset($_GET['action']) ? $_GET['action'] : '';
+$action = isset($_GET['action']) ? $_GET['action'] : 'home';
 //$login = new Login('users', 'id', 'email', 'wachtwoord', 'key');
 //if(!empty($_POST['registreren'])){
 //    $login->register($_POST['voornaam'], $_POST['achternaam'], $_POST['wachtwoord'],$_POST['telefoon'], $_POST['e-mail'], $_POST['naam-organisatie'], $_POST['website'], $_POST['type'], $_POST['plaats']);
@@ -13,19 +16,12 @@ $action = isset($_GET['action']) ? $_GET['action'] : '';
 
 include_once "views/private/header.php";
 include_once "views/private/nav.php";
-
-if($action != ''){
     if(file_exists('views/'. $action . '.php')){
         include_once 'views/'. $action . '.php';
     } else
     {
         include_once 'views/private/404.php';
     }
-} else
-{
-    include_once 'views/home.php';
-}
-
 include_once "views/private/footer.php";
 
 var_dump($action);

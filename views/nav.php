426abc747167f945dc1fedd7a9c97eae73cc3e54
indykoning <indykoning@gmail.com>
2017-03-13 13:00:25 +0100

    register/login naar nav

---
+++ views/nav.php
@@ -0,0 +1,16 @@
+<form id="registerForm" method="post">
+    <table>
+        <tr><td><label>e-mail</label></td><td><input type="text" name="username" placeholder="e-mail"></td></tr>
+        <tr><td><label>wachtwoord</label></td><td><input type="password" name="wachtwoord" placeholder="wachtwoord"></td></tr>
+        <tr><td><label>wachtwoord herhalen</label></td><td><input type="password" name="wachtwoord2" placeholder="wachtwoord herhalen"></td></tr>
+        <tr><td></td><td><input type="submit" name="registreren" value="Registreer"></td></tr>
+    </table>
+</form>
+
+<form id="loginForm" method="post">
+    <table>
+        <tr><td><label>e-mail</label></td><td><input type="text" name="username" placeholder="e-mail"></td></tr>
+        <tr><td><label>wachtwoord</label></td><td><input type="password" name="wachtwoord" placeholder="wachtwoord"></td></tr>
+        <tr><td></td><td><input type="submit" name="registreren" value="Registreer"></td></tr>
+    </table>
+</form>

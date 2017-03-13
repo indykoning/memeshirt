d9824b02c36a09a820706ddeb97e996d9aab09da
indykoning <indykoning@gmail.com>
2017-03-13 11:19:13 +0100

    home toegevoegd en basis login script

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
-require_once 'classes/Login.php';
+//require_once 'classes/Login.php';
 $action = isset($_GET['page']) ? $_GET['page'] : '';
-$login = new Login('users', 'id', 'email', 'wachtwoord', 'key');
-if(!empty($_POST['registreren'])){
-    $login->register($_POST['voornaam'], $_POST['achternaam'], $_POST['wachtwoord'],$_POST['telefoon'], $_POST['e-mail'], $_POST['naam-organisatie'], $_POST['website'], $_POST['type'], $_POST['plaats']);
-}
-if (isset($_GET['logout'])){
-    $login->logout();
-}
-define('LOGGED_IN', $login->loggedin());
+//$login = new Login('users', 'id', 'email', 'wachtwoord', 'key');
+//if(!empty($_POST['registreren'])){
+//    $login->register($_POST['voornaam'], $_POST['achternaam'], $_POST['wachtwoord'],$_POST['telefoon'], $_POST['e-mail'], $_POST['naam-organisatie'], $_POST['website'], $_POST['type'], $_POST['plaats']);
+//}
+//if (isset($_GET['logout'])){
+//    $login->logout();
+//}
+//define('LOGGED_IN', $login->loggedin());
 if($action != ''){
 if(file_exists('views/'. $action . '.php')){
 include_once 'views/'. $action . '.php';

10459dcf661976e4a8defd46d19f5aa206e36727
Chris <SSSilyTV@gmail.com>
2017-03-28 10:47:01 +0200

    css

--- views/ontwerpen.php
+++ views/ontwerpen.php
@@ -35,14 +35,14 @@ if (!empty($_POST['add_to_cart'])) {
 //    imagesavealpha($image, true);
 
 
-    $image_p = imagecreatetruecolor(3508, 2480);
+    $image_p = imagecreatetruecolor(2480, 3508);
     setTransparency($image_p, $image);
 //    imagecolortransparent($image_p, imagecolorallocate($image_p,255, 255, 255));
 //    imagealphablending($image_p, false);
 //    imagesavealpha($image_p, true);
     $width = imagesx($image);
     $height = imagesy($image);
-    imagecopyresampled($image_p, $image, 0, 0, 0, 0,3508 , 2480, $width, $height);
+    imagecopyresampled($image_p, $image, 0, 0, 0, 0,2480 , 3508, $width, $height);
 
     ob_start(); // Let's start output buffering.
     imagejpeg($image_p); //This will normally output the image, but because of ob_start(), it won't.
@@ -60,24 +60,21 @@ if (!empty($_POST['add_to_cart'])) {
     ]);
 
 //    $data = base64_encode($image_p);
-    file_put_contents($file, $data, false, $context);
+    $data = base64_encode($image_p);
+    if (file_put_contents($file, $data, false, $context) !== false) {
         $user_id = (!empty($_SESSION['ID'])) ? $_SESSION['ID'] : 'Null';
         if (empty($_SESSION['bestelling_id'])) {
             $sql = "INSERT INTO `bestelling`(`status`, `users_id`) VALUES (0, $user_id)";
             $result = $mysqli->query($sql);
-//            var_dump($sql);
-//            var_dump(mysqli_error($mysqli));
             $_SESSION['bestelling_id'] = $mysqli->insert_id;
         }
-    $xs = abs($_POST['xs']) * PRIJS_XS;
-    $s = abs($_POST['s']) * PRIJS_S;
-    $m = abs($_POST['m']) * PRIJS_M;
-    $l = abs($_POST['l']) * PRIJS_L;
-    $xl = abs($_POST['xl']) * PRIJS_XL;
-    $xxl = abs($_POST['xxl']) * PRIJS_XXL;
-    $totaal = $xs+$s+$m+$l+$xl+$xxl;
-        $sql = "INSERT INTO `images`(`filename`, `status`, `kleur` `totaal_prijs`, `xs`, `s`, `m`, `l`, `xl`, `xxl`, `bestelling_id`) VALUES ('" . $imagename . "',0, " . $_POST['shirtColor'] . " ," . $totaal . ", "  . abs($_POST['xs']) . "," . abs($_POST['s']) . "," . abs($_POST['m']) . "," . abs($_POST['l']) . "," . abs($_POST['xl']) . "," . abs($_POST['xxl']) . "," . $_SESSION['bestelling_id'] . ")";
+        $sql = "INSERT INTO `images`(`filename`, `status`, `xs`, `s`, `m`, `l`, `xl`, `xxl`, `bestelling_id`) VALUES ('" . $imagename . "',0, " . abs($_POST['xs']) . "," . abs($_POST['s']) . "," . abs($_POST['m']) . "," . abs($_POST['l']) . "," . abs($_POST['xl']) . "," . abs($_POST['xxl']) . "," . $_SESSION['bestelling_id'] . ")";
         $mysqli->query($sql);
+//    var_dump($sql);
+//    var_dump(mysqli_error($mysqli));
+    }else{
+        var_dump('err');
+    }
 }
 ?>
 <script src="js/fabric.js"></script>
@@ -91,52 +88,11 @@ if (!empty($_POST['add_to_cart'])) {
                         <div class="col-xs-12 col-sm-6 no_padding">
                             <div class="ontwerpen_links">
                                 <div class="shirt_preview" style="overflow-y: auto">
-                                    <form method="post" enctype="multipart/form-data">
-                                        <div id="step-1">
-                                            <script>function showSelectedColor(value) {
-                                                    document.getElementById('selectedColor').innerHTML = value;
-                                                };</script>
-                                            <!--stap 1-->
-                                            <div class="colorSelect">
-                                                <input style="display: none" type="radio" name="shirtColor" onchange="showSelectedColor(this.id)" value="black" id="black"><label class="KleurKiezer_label" for="black" style="background-color: black"></label>
-                                                <input style="display: none" type="radio" name="shirtColor" onchange="showSelectedColor(this.id)" value="white" id="white" checked><label class="KleurKiezer_label" for="white" style="background-color: white"></label>
-                                                <input style="display: none" type="radio" name="shirtColor" onchange="showSelectedColor(this.id)" value="gray" id="gray"><label class="KleurKiezer_label" for="gray" style="background-color: lightgray"></label>
-                                                <input style="display: none" type="radio" name="shirtColor" onchange="showSelectedColor(this.id)" value="blue" id="blue"><label class="KleurKiezer_label" for="blue" style="background-color: blue"></label>
-                                                <input style="display: none" type="radio" name="shirtColor" onchange="showSelectedColor(this.id)" value="pink" id="pink"><label class="KleurKiezer_label" for="pink" style="background-color: deeppink"></label>
-                                                <input style="display: none" type="radio" name="shirtColor" onchange="showSelectedColor(this.id)" value="red" id="red"><label class="KleurKiezer_label" for="red" style="background-color: red"></label>
-                                            </div>
-                                            <p id="selectedColor">white</p>
-                                        </div>
-                                        <div id="step-2">
-                                            <!--stap 2-->
-                                            <input type="file" id="fileUpload" accept="image/*">
 
-                                        </div>
-                                        <div id="step-3">
-                                            <!--stap 3-->
-                                            <input type="text" id="addtext"><input id="addtextBut" type="button" value="voeg toe"><br>
-                                            <!--<input type="color" id="color">-->
-                                            <div id="colordiv"></div>
-                                            <br>
-                                            <input type="range" min="5" max="150" value="40" id="size"><input type="button" id="deleteButton" value="verwijder het geselecteerde ding">
-                                            <canvas style="border: solid black" id="editor" width="842" height="595"></canvas>
-                                            <img src="" id="test" class="canvas-img">
-                                        </div>
-                                        <div id="step-4">
-                                            <!--stap 4 -->
-                                            <table>
-                                                <tr><td>xs</td><td><input type="number" value="0" name="xs"></td></tr>
-                                                <tr><td>s</td><td><input type="number" value="0" name="s"></td></tr>
-                                                <tr><td>m</td><td><input type="number" value="0" name="m"></td></tr>
-                                                <tr><td>l</td><td><input type="number" value="0" name="l"></td></tr>
-                                                <tr><td>xl</td><td><input type="number" value="0" name="xl"></td></tr>
-                                                <tr><td>xxl</td><td><input type="number" value="0" name="xxl"></td></tr>
+                                        <canvas id="editor"></canvas>
 
-                                            </table>
-                                            <input type="text" name="image" id="ImageToUpload" style="display: none">
-                                            <input type="submit" name="add_to_cart" value="Voeg toe aan winkelwagen">
-                                        </div>
-                                    </form>
+
+
                                 </div>
                             </div>
                         </div>
@@ -156,7 +112,55 @@ if (!empty($_POST['add_to_cart'])) {
                                         <div class="col-xs-3">
                                             <button onclick="changeStep(3)" type="button" class="btn btn-info btn_ontwerpproces_status h_button_status">Maat en aantal</button>
                                         </div>
+                                        <form method="post" enctype="multipart/form-data">
+                                        <div id="step-1">
+                                            <script>function showSelectedColor(value) {
+                                                    document.getElementById('selectedColor').innerHTML = value;
+                                                };</script>
+                                            <!--stap 1-->
 
+                                            <div class="colorSelect" >
+                                                <input style="display: none" type="radio" name="shirtColor" onchange="showSelectedColor(this.id)" value="black" id="black"><label class="KleurKiezer_label" for="black" style="background-color: black"></label>
+                                                <input style="display: none" type="radio" name="shirtColor" onchange="showSelectedColor(this.id)" value="white" id="white" checked><label class="KleurKiezer_label" for="white" style="background-color: white"></label>
+                                                <input style="display: none" type="radio" name="shirtColor" onchange="showSelectedColor(this.id)" value="gray" id="gray"><label class="KleurKiezer_label" for="gray" style="background-color: lightgray"></label>
+                                                <input style="display: none" type="radio" name="shirtColor" onchange="showSelectedColor(this.id)" value="blue" id="blue"><label class="KleurKiezer_label" for="blue" style="background-color: blue"></label>
+                                                <input style="display: none" type="radio" name="shirtColor" onchange="showSelectedColor(this.id)" value="pink" id="pink"><label class="KleurKiezer_label" for="pink" style="background-color: deeppink"></label>
+                                                <input style="display: none" type="radio" name="shirtColor" onchange="showSelectedColor(this.id)" value="red" id="red"><label class="KleurKiezer_label" for="red" style="background-color: red"></label>
+                                            </div>
+                                            <p id="selectedColor">white</p>
+                                        </div>
+                                        <div id="step-2">
+                                            <!--stap 2-->
+                                            <input  type="file" id="fileUpload" accept="image/*">
+
+                                        </div>
+
+                                        <div id="step-3">
+                                            <!--stap 3-->
+                                            <input type="text" id="addtext"><input id="addtextBut" type="button" value="voeg toe"><br>
+                                            <!--<input type="color" id="color">-->
+                                            <div id="colordiv"></div>
+                                            <br>
+                                            <input type="range" min="5" max="150" value="40" id="size"><input type="button" id="deleteButton" value="verwijder het geselecteerde ding">
+
+                                            <canvas style="border: solid black" id="editor" width="842" height="595"></canvas>
+                                            <img src="" id="test" class="canvas-img">
+                                        </div>
+                                        <div id="step-4">
+                                            <!--stap 4 -->
+                                            <table>
+                                                <tr><td>xs</td><td><input type="number" value="0" name="xs"></td></tr>
+                                                <tr><td>s</td><td><input type="number" value="0" name="s"></td></tr>
+                                                <tr><td>m</td><td><input type="number" value="0" name="m"></td></tr>
+                                                <tr><td>l</td><td><input type="number" value="0" name="l"></td></tr>
+                                                <tr><td>xl</td><td><input type="number" value="0" name="xl"></td></tr>
+                                                <tr><td>xxl</td><td><input type="number" value="0" name="xxl"></td></tr>
+
+                                            </table>
+                                            <input type="text" name="image" id="ImageToUpload" style="display: none">
+                                            <input type="submit"  name="add_to_cart" value="Voeg toe aan winkelwagen">
+                                        </div>
+                                        </form>
                                         <div class="col-xs-12">
                                             <button type="button" id="prev" class="btn btn-info btn_ontwerpproces_terug h_button_terug">Terug</button>
                                             <button type="button" id="next" class="btn btn-info btn_ontwerpproces_verder h_button_verder">Verder</button>
@@ -191,14 +195,6 @@ if (!empty($_POST['add_to_cart'])) {
     });</script>
 </body>
 </html>
-<style>
-    .KleurKiezer_label{
-        width: 200px; height: 200px;
-        border: solid black;
-    }
-    .colorSelect input[type="radio"]:checked+label {
-        box-shadow: 3px 3px 28px 7px rgba(0,0,0,0.75);
-    }
-</style>
+
 <script src="js/ontwerp_stappen.js"></script>
 <script src="js/canvas_script.js"></script>
